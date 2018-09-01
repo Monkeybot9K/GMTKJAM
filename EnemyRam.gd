9d570c2fa6a9fec919ce06cf49e8c92c9ea389e3
Nicolas Viegas Palermo <viegaspalermo@gmail.com>
2018-09-01 11:54:02 -0300

    Fixes Enemy Ram Behavior. - When ramming against a wall, remain inactive - When walking against a wall turn and move on

--- EnemyRam.gd
+++ EnemyRam.gd
@@ -7,13 +7,17 @@ export var movement_speed = 50
 export var ramming_speed = 400
 
 var isActive = false
+var isRamming = false
 var motion = Vector2()
 var downtime_timer = 0
 
-#Go straight toward the player's position
-func ramming_motion():
-	return (player_character.position - self.position).normalized() * ramming_speed
-	
+#If player is spotted, Activate the enemy
+func _on_Area2D_body_entered(body):	
+	for body in $ViewArea.get_overlapping_bodies():
+		if body.name == "PlayerCharacter":
+			isActive = true
+
+
 #Wander around in search of the player
 func random_motion():
 	randomize()
@@ -21,36 +25,53 @@ func random_motion():
 	var y = floor(rand_range(-1,1))
 	return Vector2(x,y).normalized() * movement_speed
 
+#Go straight toward the player's position
+func ramming_motion():
+	isRamming = true
+	return (player_character.position - self.position).normalized() * ramming_speed
+
 func _physics_process(delta):
 	#If inactive, do nothing
 	if isActive == false:
 		return
-	
-	#Update downtime if needed
+
+	#If on downtime, count down
 	if downtime_timer > 0:
 		downtime_timer -= delta
 	else:
-		#If not moving and ready, start moving toward a random heading
+		#If no longer in downtime, and no heading, start moving toward a random one
 		if motion.length() == 0:
 			motion = random_motion()
-			
-	for body in $Area2D.get_overlapping_bodies():
-		if body.name == "PlayerCharacter":
-			isActive = true
-			motion = ramming_motion()
-			break
-	
-	#Rotate toward heading
-	look_at(motion)
-	
-	#Perform current motion and, if collision occurs, engage downtime
-	if move_and_collide(motion * delta) != null:
-		downtime_timer = downtime_after_attack
-		motion = Vector2()
 
-func _on_Area2D_body_entered(body):
+	#if player is spotted, ram them
+	if !isRamming:
+		for body in $ViewArea.get_overlapping_bodies():
+			if body.name == "PlayerCharacter":
+				motion = ramming_motion()
+				break
+
+	#Rotate toward heading
 	
-	#If player is spotted, Activate
-	for body in $Area2D.get_overlapping_bodies():
-		if body.name == "PlayerCharacter":
-			isActive = true
+	if abs(motion.y) > abs(motion.x):
+		if motion.y < 0:
+			$Sprite.frame = 3
+			$ViewArea.rotation_degrees = -90
+		else:
+			$Sprite.frame = 0
+			$ViewArea.rotation_degrees = 90
+	else:
+		if motion.x < 0:
+			$Sprite.frame = 1
+			$ViewArea.rotation_degrees = 180
+		else:
+			$Sprite.frame = 2
+			$ViewArea.rotation_degrees = 0
+
+	#Perform current motion and, if collision occurs stop motion, engage downtime
+	if move_and_collide(motion * delta) != null:
+		if isRamming:
+			downtime_timer = downtime_after_attack
+			isRamming = false
+			motion = Vector2()
+		else:
+			motion = random_motion()
