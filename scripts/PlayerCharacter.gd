45af3eb764051e92320a3e843384799b9b25c0e5
Nicolas Viegas Palermo <viegaspalermo@gmail.com>
2018-09-01 14:50:22 -0300

    Reorganizes project files, fixes missing dependencies in PlayerCharacter Scene

--- scripts/PlayerCharacter.gd
+++ scripts/PlayerCharacter.gd
@@ -11,12 +11,12 @@ func _ready():
 
 func _physics_process(delta):
 	motion = Vector2()
-	
+
 	if Input.is_action_pressed("ui_left"):
 		motion.x = -movement_speed
 	elif Input.is_action_pressed("ui_right"):
 		motion.x = movement_speed
-	
+
 	if Input.is_action_pressed("ui_up"):
 		motion.y = -movement_speed
 	elif Input.is_action_pressed("ui_down"):
