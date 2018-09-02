38ea67983e405a227a079f98756ed936fe7c738a
Nicolas Viegas Palermo <viegaspalermo@gmail.com>
2018-09-02 14:27:25 -0300

    Adds asset for portal, Ability to Lock / Unlock it

--- scripts/Portal.gd
+++ scripts/Portal.gd
@@ -2,8 +2,18 @@ extends Area2D
 
 export(String, FILE, "*.tscn") var target
 
+export var locked = false
+
+func _ready():
+	toggleLocked(locked)
+
 func _physics_process(delta):
 	if target == null: return
 	for body in get_overlapping_bodies():
 		if body.name == "PlayerCharacter":
 			get_tree().change_scene(target)
+
+func toggleLocked(locked):
+	#Toggle Portal accordingly
+	$CollisionShape2D.disabled = locked
+	$Sprite.frame = 0 if locked else 1
