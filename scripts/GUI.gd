3724500e93e22a065cbd9db06ee64b134f9204fc
Carmon <elcarmon@gmail.com>
2018-09-02 13:28:09 -0300

    Add recoil to take_damage

--- scripts/GUI.gd
+++ scripts/GUI.gd
@@ -2,10 +2,8 @@ extends MarginContainer
 
 func _ready():
 	GameState.connect("update_gui", self, "update")
-	
 	update()
 	
 func update():
-	print("update_gui")
 	$HBoxContainer/Number.text = str(GameState.lives)
 	
